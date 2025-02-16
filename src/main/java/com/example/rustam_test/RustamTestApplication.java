0bf85c9d56b8d9a72b4b8bad484071f160e2635a
Rustam Khudoyan <dreamang2toplay@yandex.ru>
2025-02-16 14:37:15 +0300

    feat: Коммит инициализации

---
+++ src/main/java/com/example/rustam_test/RustamTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.rustam_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RustamTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RustamTestApplication.class, args);
+	}
+
+}
